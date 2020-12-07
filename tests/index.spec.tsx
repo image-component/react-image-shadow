ff47facdd29ce0ad205f04a309d97e9fa70acbba
xrkffgg <xrkffgg@gmail.com>
2020-12-07 09:24:54 +0800

    test: add test & lint (#4)
    * test: add test & lint
    
    * add peerDependencies
    
    * Update ci.yml
    
    * Update ci.yml
    
    * fix father version

---
+++ tests/index.spec.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable comma-dangle */
+import React from 'react';
+import { mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import ImageShadow from '../src';
+
+describe('imageShadow', () => {
+  const testSrc =
+    'https://github.com/image-tool/react-image-shadow/blob/main/image/red.png?raw=true';
+
+  it('render', () => {
+    let wrapper;
+
+    act(() => {
+      wrapper = mount(<ImageShadow src={testSrc} />);
+    });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('className', () => {
+    const wrapper = mount(<ImageShadow className="test-class" src={testSrc} />);
+    expect(wrapper.find('.test-class').length).toBeTruthy();
+  });
+
+  it('shadowBlur', () => {
+    const wrapper = mount(<ImageShadow shadowBlur={10} src={testSrc} />);
+    const node = wrapper.find('.react-image-shadow-card');
+    expect(node.prop('style')?.filter).toEqual('blur(10px)');
+  });
+
+  it('shadowHover', () => {
+    const wrapper = mount(<ImageShadow shadowHover src={testSrc} />);
+    expect(wrapper.find('.react-image-shadow-hover').length).toBeTruthy();
+  });
+
+  it('shadowRadius', () => {
+    const wrapper = mount(<ImageShadow shadowRadius={20} src={testSrc} />);
+    const node = wrapper.find('.react-image-shadow-img');
+    expect(node.prop('style')?.borderRadius).toEqual('20px');
+  });
+
+  it('style', () => {
+    const wrapper = mount(
+      <ImageShadow
+        style={{
+          color: 'red',
+        }}
+        src={testSrc}
+      />,
+    );
+    const node = wrapper.find('.react-image-shadow');
+    expect(node.prop('style')?.color).toEqual('red');
+  });
+
+  it('width', () => {
+    const wrapper = mount(<ImageShadow width={100} src={testSrc} />);
+    const node = wrapper.find('.react-image-shadow-img');
+    expect(node.prop('width')).toEqual(100);
+  });
+});
---
+++ tests/setup.ts
@@ -0,0 +1,7 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// @ts-ignore
+global.requestAnimationFrame = (cb) => setTimeout(cb, 0);
+
+Enzyme.configure({ adapter: new Adapter() });
