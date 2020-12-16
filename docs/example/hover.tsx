fac6075ffdcabafd957f62c722220e789984728c
xrkffgg <xrkffgg@gmail.com>
2020-12-16 16:41:23 +0800

    docs: update image url (#15)
    * docs: update image url
    
    * fix

--- docs/example/hover.tsx
+++ docs/example/hover.tsx
@@ -7,21 +7,19 @@ import ImageShadow from 'react-image-shadow';
 import './demo.less';
 import '../../assets/index.less';
 
+const url1 =
+  'https://github.com/image-component/gallery/blob/main/girl/3.jpg?raw=true';
+const url2 =
+  'https://github.com/image-component/gallery/blob/main/girl/6.jpg?raw=true';
+const url3 =
+  'https://github.com/image-component/gallery/blob/main/girl/8.png?raw=true';
+
 const App = () => {
   return (
     <div className="base-demo">
-      <ImageShadow
-        shadowHover
-        src="https://github.com/image-tool/react-image-shadow/blob/main/image/red.png?raw=true"
-      ></ImageShadow>
-      <ImageShadow
-        shadowHover
-        src="https://github.com/image-tool/react-image-shadow/blob/main/image/blue.jpg?raw=true"
-      ></ImageShadow>
-      <ImageShadow
-        shadowHover
-        src="https://github.com/image-tool/react-image-shadow/blob/main/image/white.jpg?raw=true"
-      ></ImageShadow>
+      <ImageShadow shadowHover src={url1}></ImageShadow>
+      <ImageShadow shadowHover src={url2}></ImageShadow>
+      <ImageShadow shadowHover src={url3}></ImageShadow>
     </div>
   );
 };
